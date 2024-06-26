bf38de1aadf59185b28fc7ad0a612d3ded12e337
NickPerlich <nicholasperlich2003@gmail.com>
2024-06-26 11:53:21 -0700

    initial push

---
+++ create_price.js
@@ -0,0 +1,18 @@
+const stripe = require('stripe')('sk_test_51PBGsUEX9Z1mjrezkop13TlvcbWCL0jqQPuSTv3MOdrPjg0c5lSnas6bOtpwaEpyAr6UrOVfsRgHuREIXtIp3Asl000mjDfXta');
+
+stripe.products.create({
+    name: 'Starter Subscription',
+    description: '12/Month subscription',
+}).then(product => {
+    stripe.prices.create({
+        unit_amount: 1200,
+        currency: 'usd',
+        recurring: {
+            interval: 'month',
+        },
+        product: product.id,
+    }).then(price => {
+        console.log('Success! Here is your starter subscription product id: ' + product.id);
+        console.log('Success! Here is your starter subscription price id: ' + price.id);
+    });
+});
